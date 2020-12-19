2485116a25c84afe70c53cb2c6f7887cf8b97959
Ed <lookatemail@gmail.com>
2020-12-19 16:54:44 +0000

    Able to get response from server back

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const app = express();
+// Helper files
 const sendMail = require('./mail.js');
-require('dotenv').config(); // Load Environment Variables from a .env File
+// From 2.0.35 NDG.prod
+const request = require('request');
+const sha512 = require('js-sha512');
+
+require('dotenv').config(); // Load Environment constiables from a .env File
 
 // Middleware
 app.use(express.static(__dirname + '/public'));
@@ -96,50 +101,138 @@ app.get('/contact', (req, res) => {
 // Contact From Submit Path
 app.post('/contact', formSubmit);
 
+// Fluid One
+app.get('/getQuotes', function (req, res) {
+  const postCode = 'SW1A0AA';
+  const addressId = '80/20';
+  const speed = req.query.speed;
+
+  console.log(postCode);
+  console.log(addressId);
+  console.log(speed);
+
+  const url =
+    '/leasedline/quote/' +
+    addressId +
+    '/' +
+    postCode +
+    '/BT_Wholesale/Fibre/none/item100mbits/item' +
+    speed +
+    'mbits/threeyears/0/0';
+  const hash = sha512(url + '16-CHECK-form-CAKE-25');
+
+  request(
+    {
+      url: 'https://api.interdns.co.uk' + encodeURI(url),
+      headers: {
+        User: 'ndgsuper',
+        Hash: hash,
+        Encryption: 'SHA-512',
+      },
+    },
+    function (error, btWholeSaleResponse, btWholeSaleBody) {
+      btWholeSaleBody.status = btWholeSaleResponse.statusCode;
+      // console.log(btWholeSaleResponse);
+      if (btWholeSaleBody.message) console.log(btWholeSaleBody.message);
+      console.log('btwholesale body:' + btWholeSaleBody);
+      console.log(btWholeSaleResponse.statusCode);
+
+      const url =
+        '/leasedline/quote/' +
+        addressId +
+        '/' +
+        postCode +
+        '/Virgin/Fibre/none/item100mbits/item' +
+        speed +
+        'mbits/threeyears/0/0';
+      const hash = sha512(url + '16-CHECK-form-CAKE-25');
+
+      request(
+        {
+          url: 'https://api.interdns.co.uk' + encodeURI(url),
+          headers: {
+            User: 'ndgsuper',
+            Hash: hash,
+            Encryption: 'SHA-512',
+          },
+        },
+        function (error, virginResponse, virginBody) {
+          virginBody.status = virginResponse.statusCode;
+          // console.log(virginResponse);
+          if (virginBody.message) console.log(virginBody.message);
+          console.log('virgin body:' + virginBody);
+          console.log(virginResponse.statusCode);
+
+          const url =
+            '/leasedline/quote/' +
+            addressId +
+            '/' +
+            postCode +
+            '/TalkTalk/Fibre/none/item100mbits/item' +
+            speed +
+            'mbits/threeyears/0/0';
+          const hash = sha512(url + '16-CHECK-form-CAKE-25');
+
+          request(
+            {
+              url: 'https://api.interdns.co.uk' + encodeURI(url),
+              headers: {
+                User: 'ndgsuper',
+                Hash: hash,
+                Encryption: 'SHA-512',
+              },
+            },
+            function (error, talkTalkResponse, talkTalkBody) {
+              talkTalkBody.status = talkTalkResponse.statusCode;
+              // console.log(talkTalkResponse);
+              if (talkTalkBody.message) console.log(talkTalkBody.message);
+              console.log('talktalk body:' + talkTalkBody);
+              console.log(talkTalkResponse.statusCode);
+
+              const url =
+                '/leasedline/quote/' +
+                addressId +
+                '/' +
+                postCode +
+                '/CityFibre/Fibre/none/item100mbits/item' +
+                speed +
+                'mbits/threeyears/0/0';
+              const hash = sha512(url + '16-CHECK-form-CAKE-25');
+
+              request(
+                {
+                  url: 'https://api.interdns.co.uk' + encodeURI(url),
+                  headers: {
+                    User: 'ndgsuper',
+                    Hash: hash,
+                    Encryption: 'SHA-512',
+                  },
+                },
+                function (error, cityFibreResponse, cityFibreBody) {
+                  cityFibreBody.status = cityFibreResponse.statusCode;
+                  // console.log(cityFibreResponse);
+                  console.log('cityfibre body:' + cityFibreBody);
+                  if (cityFibreBody.message) console.log(cityFibreBody.message);
+                  console.log(cityFibreResponse.statusCode);
+
+                  const finalResponse = {
+                    btWholeSale: btWholeSaleBody,
+                    virgin: virginBody,
+                    talkTalk: talkTalkBody,
+                    cityFibre: cityFibreBody,
+                  };
+                  console.log(finalResponse);
+                  res.status(200).json(finalResponse);
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
 // Server Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
-
-// app.get('/freshsales', (req, res) => {
-//   // const PASSWORD = 'dummyPassword';
-//   // var API_KEY = 'weUr7kNI1zueQZ66vOcl'; // Test Account
-//   // const FD_ENDPOINT = 'newaccount1608116901000';
-//   // // const FD_ENDPOINT = 'ndgtechnologylimited'; // NDG Account
-//   // // const API_KEY = 'jlPlNkcvQ7DRkb6N9tZ'; // NDG Account
-//   // let PATH = '/api/v2/tickets';
-//   // const URL = `https://${FD_ENDPOINT}.freshdesk.com/${PATH}`;
-//   // const enocoding_method = 'base64';
-//   // const auth =
-//   //   'Basic ' + new Buffer.from(API_KEY + ':' + 'X').toString(enocoding_method);
-//   // console.log(auth);
-
-//   // let freshDesc;
-
-//   // async function asyncFreshDesk() {
-//   //   console.log('Making a Call To FreshDesc');
-
-//   //   const defaultOptions = {
-//   //     method: 'GET',
-//   //     mode: 'cors',
-//   //     headers: {
-//   //       Authorization: auth,
-//   //       'Content-Type': 'application/json',
-//   //       'Access-Control-Allow-Origin': '*',
-//   //     },
-//   //   };
-
-//   //   const freshDescReq = new Request(URL, defaultOptions);
-
-//   //   const res = await fetch(freshDescReq);
-//   //   const freshDesc = await res.json();
-
-//   //   const TICKET = 0;
-
-//   //   console.log(freshDesc);
-//   //   console.log(freshDesc[TICKET].subject);
-//   //   console.log(freshDesc[TICKET].type);
-//   //   console.log(freshDesc[TICKET].updated_at);
-//   // }
-
-//   res.render('freshSales.html');
-// });
