f2e33b82a4c29504b489942f1cf696635c3ca794
Ed <lookatemail@gmail.com>
2020-12-19 11:47:46 +0000

    Refactor freshSales.js

--- public/JS/freshSales.js
+++ public/JS/freshSales.js
@@ -1,38 +1,33 @@
+console.log('Loading FreshSales...');
+
 async function asyncFreshDesk() {
-  const PASSWORD = 'dummyPassword';
-  var API_KEY = 'weUr7kNI1zueQZ66vOcl'; // Test Account
-  const FD_ENDPOINT = 'newaccount1608116901000'; // Test Account
-  // const API_KEY = 'jlPlNkcvQ7DRkb6N9tZ'; // NDG Account
-  // const FD_ENDPOINT = 'ndgtechnologylimited'; // NDG Account
+  // const TOKEN = 'Basic amxQbE5rY3ZRN0RSa2I2Tjl0WjpY'; // DG Account
+  const TOKEN = 'Basic d2VVcjdrTkkxenVlUVo2NnZPY2w6WA==';
+  const FD_ENDPOINT = 'newaccount1608116901000';
   let PATH = '/api/v2/tickets';
   const URL = `https://${FD_ENDPOINT}.freshdesk.com/${PATH}`;
-  const ENCODING_METHOD = 'base64';
-  const auth =
-    'Basic ' + new Buffer.from(API_KEY + ':' + 'X').toString(ENCODING_METHOD);
-  console.log(auth);
-  let freshDesc;
-  async function asyncFreshDesk() {
-    console.log('Making a Call To FreshDesc');
-    const defaultOptions = {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        Authorization: auth,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-    };
-    const freshDescReq = new Request(URL, defaultOptions);
-    const res = await fetch(freshDescReq);
-    const freshDesc = await res.json();
-    const TICKET = 0;
-    console.log(freshDesc);
-    console.log(freshDesc[TICKET].subject);
-    console.log(freshDesc[TICKET].type);
-    console.log(freshDesc[TICKET].updated_at);
-  }
 
-  return 'End';
+  console.log('Making a Call To FreshDesc');
+
+  const defaultOptions = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Authorization: TOKEN,
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+
+  const freshDescReq = new Request(URL, defaultOptions);
+
+  const res = await fetch(freshDescReq);
+  const data = await res.json();
+
+  const TICKET = 1;
+
+  console.log(data);
+  console.log(data[TICKET].subject);
+  console.log(data[TICKET].type);
+  console.log(data[TICKET].updated_at);
 }
-
-module.exports = asyncFreshDesk;
