4574a99de9051d260a78dcea7b8ca7a2d9e03719
Ed <lookatemail@gmail.com>
2020-12-19 19:57:34 +0000

    Add from validation and post submit

--- public/JS/fluidOne.js
+++ public/JS/fluidOne.js
@@ -1,1 +1,18 @@
 console.log('Loading FluidOne...');
+
+const fluidOne = document.querySelector('#fluidForm');
+fluidOne.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const postCode = document.querySelector('.postCode').value;
+  const email = document.querySelector('.email').value;
+  console.log(postCode);
+  console.log(email);
+
+  const form = e.target;
+  // error handling
+  if (postCode == '' || email == '') {
+    console.log('Form Error');
+  } else {
+    form.submit();
+  }
+});
